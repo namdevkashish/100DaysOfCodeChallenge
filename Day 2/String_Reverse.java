e32dd8ec2323870cea6e2cd964ee45d6e4df0599
namdevkashish <114773155+namdevkashish@users.noreply.github.com>
2022-10-01 21:29:24 +0530

    Move the cursor here to view the question
    A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
    Given a string  A, print Yes if it is a palindrome, print No otherwise.
    
    Constraints
    
    A  will consist at most 50 lower case English letters.
    
    Sample Input
    madam
    
    Sample Output
    Yes

---
+++ Day 2/String_Reverse.java
@@ -0,0 +1,30 @@
+# 100DaysOfCodeChallenge
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        if(isPalindrome(str)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+    public static boolean isPalindrome(String str){
+        int l=0,r=str.length()-1;
+        while(l < r) {
+            if (str.charAt(l) != str.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
+
